88ed6c602de6d881f034c35904dc1d9258a14284
Badhusha Shaji <Badhusha.shaji22@gmail.com>
2024-02-19 10:58:00 +0530

    tailwing config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import "bootstrap/dist/css/bootstrap.css";
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import './index.css'
+
 createApp(App).mount('#app')
 
-import "bootstrap/dist/js/bootstrap.js";
