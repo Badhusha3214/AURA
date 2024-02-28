f95fb7a55345fd77231a92641f8baa4b70db95f7
Badhusha Shaji <Badhusha.shaji22@gmail.com>
2024-02-29 01:56:38 +0530

    nil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 
 <template>
+    <navbar />
+
     <DashboardLayout>
 
         <Calender />
@@ -13,13 +15,15 @@
     import DashboardLayout from '@/layouts/DashboardLayout.vue'
     import CentralCircle from '@/components/CentralCircle.vue'
     import Calender from '@/components/Calender.vue'
+    import navbar from '@/components/Navbar.vue'
     
     export default {
         name: 'HomeView',
         components: {
             DashboardLayout,
             Calender,
-            CentralCircle
+            CentralCircle,
+            navbar
         }
     }
 </script>
