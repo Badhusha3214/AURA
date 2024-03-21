3e69125d79e766997ef2a50c88ec271e6a5633e8
Badhusha Shaji <Badhusha.shaji22@gmail.com>
2024-03-21 16:30:11 +0530

    added calender

--- src/components/Calender.vue
+++ src/components/Calender.vue
@@ -1,47 +1,108 @@
 <template>
- 
-    <Datepicker class="xyz w-72  text-center border-pink-500 rounded-3xl border-2 p-4 bg-white" v-model="picked" />
-
+  <div class="flex flex-col items-center">
+    <div class="bg-white rounded-md shadow p-4">
+      <div class="flex justify-between items-center mb-4">
+        <button class="text-gray-500 hover:text-gray-700" @click="prevMonth">
+          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+          </svg>
+        </button>
+        <div class="text-lg font-semibold">{{ currentMonthYear }}</div>
+        <button class="text-gray-500 hover:text-gray-700" @click="nextMonth">
+          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+          </svg>
+        </button>
+      </div>
+      <div class="grid grid-cols-7 gap-1">
+        <div v-for="day in dayNames" :key="day" class="text-xs text-gray-500 text-center">
+          {{ day }}
+        </div>
+      </div>
+      <div class="grid grid-cols-7 gap-1">
+        <div
+          v-for="day in daysInMonth"
+          :key="day.date"
+          class="text-center py-1"
+          :class="{
+            'text-gray-300': !day.date.getMonth() === currentDate.getMonth(),
+            'text-gray-700 font-semibold bg-gray-200 rounded-full w-8 h-8 mx-auto': day.isToday,
+            'text-blue-500 font-semibold bg-blue-100 rounded-full w-8 h-8 mx-auto': day.date === selectedDate,
+          }"
+        >
+          <button @click="selectDate(day.date)" class="focus:outline-none w-full h-full">
+            {{ day.day }}
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="mt-4 text-gray-700 font-semibold">
+      Selected Date: {{ formattedSelectedDate }}
+    </div>
+  </div>
 </template>
-  
 
 <script>
-    import Datepicker from 'vue3-datepicker'
-    import { ref } from 'vue'
-
 export default {
-    name: 'Calender',
-    data() {
-        const picked = ref(new Date())
-        console.log(picked)
-      return {
-        // picked // Holds the value entered by the user
+  name: 'Calender',
+  data() {
+    return {
+      currentDate: new Date(),
+      selectedDate: null,
+      dayNames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    };
+  },
+  computed: {
+    currentMonthYear() {
+      const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ];
+      const month = months[this.currentDate.getMonth()];
+      const year = this.currentDate.getFullYear();
+      return `${month} ${year}`;
+    },
+    daysInMonth() {
+      const daysInMonth = [];
+      const firstDay = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth(), 1);
+      const lastDay = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 0);
+      const startingDay = firstDay.getDay();
+      let date = startingDay === 0 ? firstDay : new Date(firstDay.setDate(firstDay.getDate() - startingDay));
+      while (date <= lastDay) {
+        const day = {
+          date: new Date(date),
+          day: date.getDate(),
+          isToday: date.toDateString() === new Date().toDateString(),
+        };
+        daysInMonth.push(day);
+        date.setDate(date.getDate() + 1);
       }
-    }
-  }
+      return daysInMonth;
+    },
+    formattedSelectedDate() {
+      return this.selectedDate ? this.selectedDate.toLocaleDateString() : '';
+    },
+  },
+  methods: {
+    prevMonth() {
+      this.currentDate.setMonth(this.currentDate.getMonth() - 1);
+    },
+    nextMonth() {
+      this.currentDate.setMonth(this.currentDate.getMonth() + 1);
+    },
+    selectDate(date) {
+      this.selectedDate = date;
+    },
+  },
+};
 </script>
-
-<style>
-.xyz{
-    --vdp-bg-color	: #f06;
-    --vdp-bg-color	:#fff;	                           /* color */
-    --vdp-text-color:#000;	                           /*color */
-    --vdp-box-shadow: 0 4px 10px 0 rgba(128, 144, 160, 0.1), 0 0 1px 0 rgba(128, 144, 160, 0.81);	/* shadow */
-    --vdp-border-radius:3px;	                        /* size*/
-    --vdp-heading-size:2.5em;	                        /* size*/
-    --vdp-heading-weight:bold;	                        /* weight*/
-    --vdp-heading-hover-color:#eeeeee;	               /* color*/
-    --vdp-arrow-color:black;	                       /* color*/
-    --vdp-elem-color:black;	                       /* color*/
-    --vdp-disabled-color:#d5d9e0;	                   /* color*/
-    --vdp-hover-color:#fff;	                       /* color*/
-    --vdp-hover-bg-color:#0baf74;	                   /* color*/
-    --vdp-selected-color:#fff;	                       /* color*/
-    --vdp-selected-bg-color:#0baf74;	               /* color*/
-    --vdp-current-date-outline-color:#888888;         /* color*/
-    --vdp-current-date-font-weight:bold;	            /* weight*/
-    --vdp-elem-font-size:0.8em;	                        /* size*/
-    --vdp-elem-border-radius:3px;       	            /* size*/
-     --vdp-divider-color:#d5d9e0;	                   /*color */ 
-}
-</style>
