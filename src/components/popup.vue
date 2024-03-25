9c7df3e48c75650a4dd2f781e93074e8012a1e21
Badhusha Shaji <Badhusha.shaji22@gmail.com>
2024-03-26 02:38:15 +0530

    popup

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -1,4 +1,8 @@
 <template>
+
+<!-- test  -->
+
+
     <div class="relative">
       <button @click="togglePopup" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Toggle Popup
