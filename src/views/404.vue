d8305329f801670c862d881a9cef4f16f334a3b9
Badhusha Shaji <Badhusha.shaji22@gmail.com>
2024-03-26 03:28:22 +0530

    404

---
+++ src/views/404.vue
@@ -0,0 +1,18 @@
+<template>
+<section class="">
+            <div class="flex h-screen mx-auto w-screen p-4 items-center justify-center">
+
+                <div class="w-full lg:w-1/3">
+                    <img class="w-full h-full" src="../../public/assets/404.svg" alt="404" />
+                </div>
+
+            </div>
+        </section>
+</template>
+      
+<script>
+    export default {
+      name: '404',
+    };
+</script>
+ 
