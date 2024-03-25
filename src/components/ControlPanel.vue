554c01a8e319ea0398e36491966a5dcb17e941d2
Badhusha Shaji <Badhusha.shaji22@gmail.com>
2024-03-26 02:58:19 +0530

    popup

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -17,8 +17,8 @@
       </div>
       <div class="control-panel__plus-btn -mt-10">
         <button
-          class="bg-primary text-white rounded-full -mt-10 w-14 h-14"
-          @click="showPopup = true"
+          class="bg-primary text-white rounded-full w-14 h-14"
+          @click="togglePopup"
         >
           <span class="text-2xl icon-ic-add"></span>
         </button>
@@ -27,27 +27,31 @@
       <!-- Slide Popup -->
       <div
         v-if="showPopup"
-        class="fixed inset-x-0 top-0 z-50 flex items-start justify-center pt-96 "
+        class="fixed inset-x-0 bottom-20 z-50 flex justify-center"
       >
         <div
-          class="absolute inset-0 "
+          class="absolute inset-0 bg-black opacity-50"
           @click="showPopup = false"
         ></div>
         <div
-          class="bg-secondary   rounded-lg p-6 mx-auto relative z-10 transition-all duration-300 transform"
-          :class="{ 'translate-y-0': showPopup, '-translate-y-full': !showPopup }"
+          class="bg-secondary rounded-t-lg p-6 w-full max-w-screen-sm mx-auto relative z-10 transition-all duration-300 transform"
+          :class="{
+            'translate-y-0': showPopup,
+            'translate-y-full': !showPopup,
+          }"
         >
-          <div class="grid  grid-cols-4 gap-8 w-max">
-            <div
+          <div class="grid grid-cols-4 gap-8">
+            <router-link
               v-for="icon in icons"
               :key="icon.name"
-              class="flex flex-col  items-center"
+              :to="icon.route"
+              class="flex flex-col items-center"
             >
-              <div class="bg-gray-200  rounded-full p-8">
-                <span :class="icon.iconClass" class="text-2xl"></span>
+              <div class="w-10 h-10 rounded-full overflow-hidden">
+                <img :src="icon.iconSrc" :alt="icon.name" class="w-full h-full object-cover" />
               </div>
-              <span class="text-sm  mt-2">{{ icon.name }}</span>
-            </div>
+              <span class="text-sm mt-2">{{ icon.name }}</span>
+            </router-link>
           </div>
         </div>
       </div>
@@ -68,23 +72,28 @@
         activeControl: null,
         showPopup: false,
         icons: [
-          { name: 'Icon 1', iconClass: 'icon-ic-icon1' },
-          { name: 'Icon 2', iconClass: 'icon-ic-icon2' },
-          { name: 'Icon 3', iconClass: 'icon-ic-icon3' },
-          { name: 'Icon 4', iconClass: 'icon-ic-icon4' },
-          { name: 'Icon 5', iconClass: 'icon-ic-icon5' },
-          { name: 'Icon 6', iconClass: 'icon-ic-icon6' },
-          { name: 'Icon 7', iconClass: 'icon-ic-icon7' },
-          { name: 'Icon 8', iconClass: 'icon-ic-icon8' },
-          { name: 'Icon 9', iconClass: 'icon-ic-icon9' },
-          { name: 'Icon 10', iconClass: 'icon-ic-icon10' },
-          { name: 'Icon 11', iconClass: 'icon-ic-icon11' },
-          { name: 'Icon 12', iconClass: 'icon-ic-icon12' }
+          { name: 'Icon 1', iconSrc: 'path/to/icon1.png', route: '/icon1' },
+          { name: 'Icon 2', iconSrc: 'path/to/icon2.png', route: '/icon2' },
+          { name: 'Icon 3', iconSrc: 'path/to/icon3.png', route: '/icon3' },
+          { name: 'Icon 4', iconSrc: 'path/to/icon4.png', route: '/icon4' },
+          { name: 'Icon 5', iconSrc: 'path/to/icon5.png', route: '/icon5' },
+          { name: 'Icon 6', iconSrc: 'path/to/icon6.png', route: '/icon6' },
+          { name: 'Icon 7', iconSrc: 'path/to/icon7.png', route: '/icon7' },
+          { name: 'Icon 8', iconSrc: 'path/to/icon8.png', route: '/icon8' },
+          { name: 'Icon 9', iconSrc: 'path/to/icon9.png', route: '/icon9' },
+          { name: 'Icon 10', iconSrc: 'path/to/icon10.png', route: '/icon10' },
+          { name: 'Icon 11', iconSrc: 'path/to/icon11.png', route: '/icon11' },
+          { name: 'Icon 12', iconSrc: 'path/to/icon12.png', route: '/icon12' }
         ]
       }
     },
     mounted() {
       this.activeControl = this.$route.path.split('/')[1]
+    },
+    methods: {
+      togglePopup() {
+        this.showPopup = !this.showPopup
+      }
     }
   }
   </script>
