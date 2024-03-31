cd5fe7e3bf6f257c905019e6fbd04554d6475e69
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-04-01 01:37:29 +0530

    changed the color and text of the dropdown button in the profile section

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -18,8 +18,8 @@
 
             <!-- drop down button -->
             <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
-                class="text-black w-64 bg-gray-200 hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-tertiary font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-                type="button">Dropdown button <svg class="ml-20 w-2.5 h-2.5 ms-3" aria-hidden="true"
+                class="text-black w-64 bg-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+                type="button">Monthly analytics downloads<svg class="ml-20 w-2.5 h-2.5 ms-3" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="m1 1 4 4 4-4" />
