74113941def5b7ef331e64e22b2e55cbf8a11c88
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-05-11 23:39:46 +0530

    added date adder

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -3,9 +3,9 @@
     <div class="control-panel__items">
       <router-link v-for="control in controls" :key="control.name" :to="control.route" class="control-panel__item"
         :class="{
-        'text-primary': activeControl === control.name.toLowerCase(),
-        'text-accent': activeControl !== control.name.toLowerCase(),
-      }">
+          'text-primary': activeControl === control.name.toLowerCase(),
+          'text-accent': activeControl !== control.name.toLowerCase(),
+        }">
         <span :class="control.icon" class="text-2xl"></span>
         <span class="text-xs font-semibold">{{ control.name }}</span>
       </router-link>
@@ -24,9 +24,9 @@
       <div
         class="bg-white rounded-t-lg p-6 w-full max-w-screen-sm mx-auto relative z-10 transition-all duration-300 transform"
         :class="{
-        'translate-y-0': showPopup,
-        'translate-y-full': !showPopup,
-      }">
+          'translate-y-0': showPopup,
+          'translate-y-full': !showPopup,
+        }">
         <h2 class="text-xl font-bold mb-4">Quick View Options</h2>
         <hr class="h-px mb-2 bg-gray-500 border-0 dark:bg-gray-700">
 
@@ -42,14 +42,72 @@
     </div>
 
     <!-- Date Picker -->
+    <!-- STARTING  -->
     <div v-if="showDatePicker" class="fixed inset-0 z-50 flex justify-center items-center">
       <div class="absolute inset-0 bg-black opacity-50" @click="showDatePicker = false"></div>
       <div class="bg-white rounded-lg p-6 relative z-10">
-        <h2 class="text-xl font-bold mb-4">Select Date</h2>
-        <date-picker v-model="selectedDate"></date-picker>
+        <h2 class="text-xl font-bold mb-4">Select Your Starting Date</h2>
+        <div v-if="showError" class="text-red-500 mt-2">
+          Please enter a date on or before today.
+        </div>
+        <div class="mr-4">
+          <input type="date" id="datePicker" class="form-input rounded-xl bg-gray-50" v-model="selectedDate">
+        </div>
         <button class="mt-4 bg-primary text-white rounded-md px-4 py-2" @click="submitDate">Submit</button>
       </div>
     </div>
+
+    <!-- ENDING -->
+    <div v-if="showendingDatePicker" class="fixed inset-0 z-50 flex justify-center items-center">
+      <div class="absolute inset-0 bg-black opacity-50" @click="showDatePicker = false"></div>
+      <div class="bg-white rounded-lg p-6 relative z-10">
+        <h2 class="text-xl font-bold mb-4">Select Your Ending Date</h2>
+        <div v-if="showError" class="text-red-500 mt-2">
+          Please enter a date on or before today.
+        </div>
+        <div class="mr-4">
+          <input type="date" id="datePicker" class="form-input rounded-xl bg-gray-50" v-model="selectedDate">
+        </div>
+        <button class="mt-4 bg-primary text-white rounded-md px-4 py-2" @click="submitendDate">Submit</button>
+      </div>
+    </div>
+
+    <!-- Mood -->
+    <div v-if="yourMood" class="fixed inset-0 z-50 flex justify-center items-center">
+      <div class="absolute inset-0 bg-black opacity-50" @click="showDatePicker = false"></div>
+      <div class="bg-white rounded-lg p-6 relative z-10">
+        <h2 class="text-xl font-bold mb-4">About Your Moods</h2>
+        <p>During the periods time</p>
+        <textarea v-model="newNoteContent"
+          class=" border-white w-full rounded-md px-4 py-2  placeholder-gray-400  text-base h-48"
+          placeholder="Content"></textarea>
+        <button class="mt-4 bg-primary text-white rounded-md px-4 py-2" @click="submitmood">Submit</button>
+      </div>
+    </div>
+
+    <!-- Bleeding Status -->
+    <div v-if="bleedingStatus" class="fixed inset-0 z-50 flex justify-center items-center">
+      <div class="absolute inset-0 bg-black opacity-50" @click="bleedingStatus = false"></div>
+      <div class="bg-white rounded-lg p-6 relative z-10 flex flex-col items-center">
+        <h2 class="text-xl font-bold mb-4">How was your bleeding</h2>
+        <div class="flex flex-col items-start mb-4">
+          <div class="flex items-center mb-2">
+            <input id="heavy" class="peer/draft" type="radio" name="status" value="heavy" v-model="bleedingLevel" />
+            <label for="heavy" class="peer-checked/draft:text-sky-500 ml-2">Heavy</label>
+          </div>
+          <div class="flex items-center mb-2">
+            <input id="medium" class="peer/draft" type="radio" name="status" value="medium" v-model="bleedingLevel" />
+            <label for="medium" class="peer-checked/draft:text-sky-500 ml-2">Medium</label>
+          </div>
+          <div class="flex items-center">
+            <input id="low" class="peer/draft" type="radio" name="status" value="low" v-model="bleedingLevel" />
+            <label for="low" class="peer-checked/draft:text-sky-500 ml-2">Low</label>
+          </div>
+        </div>
+        <button class="mt-4 bg-primary text-white rounded-md px-4 py-2" @click="submitBleedingLevel">Submit</button>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -59,7 +117,7 @@
 export default {
   name: 'ControlPanel',
   components: {
-    
+
   },
   data() {
     return {
@@ -72,7 +130,12 @@ export default {
       activeControl: null,
       showPopup: false,
       showDatePicker: false,
+      showendingDatePicker: false,
+      yourMood: false,
       selectedDate: null,
+      newNoteContent: null,
+      bleedingStatus: false,
+      bleedingLevel: null,
       icons: [
         { name: 'Add starting date', iconSrc: '/assets/Icons/Group 59.png' },
         { name: 'Add ending date', iconSrc: '/assets/Icons/Group 60.png' },
@@ -105,11 +168,65 @@ export default {
       if (icon.name === 'Add starting date') {
         this.showDatePicker = true;
       }
+      if (icon.name === 'Add ending date') {
+        this.showendingDatePicker = true;
+      }
+      if (icon.name === 'Your Mood') {
+        this.yourMood = true;
+      }
+      if (icon.name === 'Bleeding status') {
+        this.bleedingStatus = true;
+      }
     },
     submitDate() {
       // Handle the selected date here
-      console.log('Selected Date:', this.selectedDate);
+      console.log('last_period_start:', this.selectedDate);
+      localStorage.setItem('last_period_start', this.selectedDate);
+      localStorage.setItem('last_period_end', "");
       this.showDatePicker = false;
+    },
+    submitendDate() {
+      // Handle the selected date here
+      console.log('last_period_end', this.selectedDate);
+      localStorage.setItem('last_period_end', this.selectedDate);
+      this.showendingDatePicker = false;
+    },
+    submitmood() {
+      // console.log("kitti mone kiti");
+      localStorage.setItem('Moods', this.newNoteContent);
+      this.yourMood = false;
+    },
+    submitBleedingLevel() {
+      // Handle the selected bleeding level here
+      console.log('Bleeding Level:', this.bleedingLevel);
+      // Reset or close the modal as needed
+      this.bleedingStatus = false;
+    }
+  },
+  watch: {
+    selectedDate(newDate) {
+      if (newDate) {
+        const selectedDateObj = new Date(newDate);
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); // Set time to 00:00:00 to ensure fair comparison
+
+        // Check if the selected date is after today
+        if (selectedDateObj > today) {
+          this.selectedDate = null; // Reset the selected date
+          this.showError = true; // Show an error message or notification
+        } else {
+          this.showError = false; // Hide the error message or notification
+        }
+      }
+    }
+  },
+  computed: {
+    maxDate() {
+      const today = new Date();
+      const year = today.getFullYear();
+      const month = String(today.getMonth() + 1).padStart(2, '0');
+      const day = String(today.getDate()).padStart(2, '0');
+      return `${year}-${month}-${day}`;
     }
   }
 }
